9aefcb510224a32daeebc54a6841a197553fa340
Stephan Janosch <janosch@mpi-cbg.de>
2019-07-15 17:34:03 +0200

    added credentials file check creating data element group

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+CREDENTIALSFILE="$HOME/.labfolder_credentials.sh"
 TOKENFILE="$HOME/.labfolder_token.sh"
 PROJECTID="3358"
 
-#TODO: check for credential file
-source $HOME/.labfolder_credentials.sh
+if [ ! -f $CREDENTIALSFILE ]
+then
+    echo "did not found $CREDENTIALSFILE. Please check https://gitlab.gwdg.de/labfolder-api-tools/git-hook/blob/master/README.md"
+    exit 1
+else
+    source $CREDENTIALSFILE
+fi
 
 if [  -f $TOKENFILE ]
 then
@@ -28,8 +34,8 @@ then
     if [[ $MESSAGE == "Invalid token" ]]
     then
         #invalid token, so get a new one
-        echo "aquiring new token"
-        echo "user provided: $USER"
+        echo "No valid labfolder token found. Aquriring new one."
+        echo "user provided: $USER. Please enter password on $LABFOLDER_HOST"
         read -s -p "password: " PASSWORD
         POSTAUTH="{\"user\":\"$USER\",\"password\":\"$PASSWORD\"}"
         RESULT=`curl -s -H "Content-Type: application/json"  -X POST -d "$POSTAUTH" https://$LABFOLDER_HOST/api/v2/auth/login`
@@ -42,8 +48,28 @@ fi
 # we have a valid $TOKEN
 
 #create entry
-POSTJSON="{\"title\": \"test entry 1\",\"project_id\": \"$PROJECTID\"}"
-ENTRY=`curl -s -u "$TOKEN:" -H "Content-Type: application/json" -X POST -d "$POSTJSON" "https://$LABFOLDER_HOST/api/v2/entries"`
-ENTRID=`echo $ENTRY | jq -r '.id'`
+#POSTJSON="{\"title\": \"test entry 1\",\"project_id\": \"$PROJECTID\"}"
+#ENTRY=`curl -s -f -u "$TOKEN:" -H "Content-Type: application/json" -X POST -d "$POSTJSON" "https://$LABFOLDER_HOST/api/v2/entries"`
+#if [ $? != 0 ]
+#then
+#    echo "could not create labfolder entry"
+#    exit 1
+#fi
+#ENTRID=`echo $ENTRY | jq -r '.id'`
 
-echo $ENTRID
+ENTRID=133379
+
+DESC="bla"
+POSTJSON="{\"entry_id\": \"$ENTRID\",  \"data_elements\": [ \
+    { \
+      \"type\": \"DATA_ELEMENT_GROUP\", \
+      \"title\": \"Commit\", \
+      \"children\": [ \
+{\"type\": \"DESCRIPTIVE_DATA_ELEMENT\",\"title\": \"Descriptive XYZ\",\"description\": \"$DESC\"}, \
+{\"type\": \"DESCRIPTIVE_DATA_ELEMENT\",\"title\": \"Descriptive XYZ\",\"description\": \"$DESC\"} \
+      ]\
+    }\
+]}"
+
+
+curl -s -f -u "$TOKEN:" -H "Content-Type: application/json" -X POST -d "$POSTJSON" "https://$LABFOLDER_HOST/api/v2/elements/data"
