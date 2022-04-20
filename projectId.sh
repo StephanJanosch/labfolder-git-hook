6cf9dee42bfd20253d22d35475585ff9644b01a8
Stephan Janosch <janosch@mpi-cbg.de>
2022-04-20 15:58:12 +0200

    added 200 character limit to log message because of Labfolder limits

--- projectId.sh
+++ projectId.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
 PROJECTID=1222
-
-#more on the version with extension,Lap on the sphere with analytical initial condition and extension of the Lap,jupyter notebooks to plot scalar and vectorial quantities of the open surface benchmark,tested Euclidean Laplacian on the sphere with different settings: extending initial qty and/or using analytical qty, extending (or not) the Laplacian
-#more on the version with extension,Lap on the sphere with analytical initial condition and extension of the Lap,jupyter notebooks to plot scalar and vectorial quantities of the open surface benchmark,tested Euclidean Laplacian on the sphere with different
-  #more on the version with extension,Lap on the sphere with analytical initial condition and extension of the Lap,jupyter notebooks to plot scalar and vectorial quantities of the open surface benchmar
